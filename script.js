a0aab792d2193365b6cdb4a601a09fee61a0b1ce
Dochoaf <danielochoaflorez@gmail.com>
2026-03-30 02:02:17 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function allowAccess() {
-  document.getElementById("ageModal").style.display = "none";
-}
-
-function denyAccess() {
-  alert("No puedes ingresar a este sitio");
-  window.location.href = "https://www.google.com";
-}
